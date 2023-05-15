5fe7c9e90b9730b124f5645fb5a2a8990e693167
essss <sousououederni@gmail.com>
2023-05-15 22:52:39 +0100

    set up the posts

---
+++ backend/controllers/Post.controller.ts
@@ -0,0 +1,12 @@
+import { Post } from "../models/Post.model";
+import mongoose from "mongoose";
+
+//get all posts
+export const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
---
+++ backend/models/Post.model.ts
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+export const PostSchema = new Schema({
+  createdBy: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  genres: {
+    type: Schema.Types.ObjectId,
+    ref: "Genre",
+  },
+  isbn: {
+    type: String,
+    required: true,
+  },
+  condition: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const Post = mongoose.model("Post", PostSchema);
