059b652269072713e40c2904ca5b07b4075f86c1
Khaledtf Labeb <khaledtf19@gmail.com>
2023-05-16 19:53:19 +0300

    adding post component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,14 @@
+import Post from "@/components/post/Post";
 import { SignInButton, UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs";
+
 
 export default function Home() {
   const { userId } = auth();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
+    <main className="flex min-h-screen flex-col items-center justify-start p-24 gap-5 ">
       {userId ? <UserButton /> : <SignInButton />}
+      <div><Post /></div>
     </main>
   );
 }
---
+++ frontend/src/types/post.type.ts
@@ -0,0 +1,11 @@
+type Posti = {
+  id: string;
+  createdBy: string;
+  createdAt: Date;
+  updatedAt: Date;
+  tile: string;
+  content: string;
+  imgs: string[];
+  condition: number;
+  price: number;
+}
