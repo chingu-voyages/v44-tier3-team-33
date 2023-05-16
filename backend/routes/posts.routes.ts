0a94e24f660539eccb6554619561eae422fca059
essss <sousououederni@gmail.com>
2023-05-16 14:01:25 +0100

    create post

--- backend/routes/posts.routes.ts
+++ backend/routes/posts.routes.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { getAllPosts } from "../controllers/Post.controller";
+import { getAllPosts, createPost } from "../controllers/Post.controller";
 
 const postRouter = express.Router();
 
 postRouter.get("/", getAllPosts);
+postRouter.post("/create", createPost);
 
 export { postRouter };
