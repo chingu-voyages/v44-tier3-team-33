7036655e92f3b163780248502d28f0b2cd9d7dc2
essss <sousououederni@gmail.com>
2023-05-17 15:06:10 +0100

    added new functions to posts

--- backend/controllers/Post.controller.ts
+++ backend/controllers/Post.controller.ts
@@ -3,15 +3,11 @@ import { Request, Response } from "express";
 import { Genre } from "../models/Genre.model";
 import { users } from "@clerk/clerk-sdk-node";
 
-interface PublicMetadata {
-  posts: any[]; // Update the type of posts as needed
-  "fav-genres": string[]; // Update the type of fav-genres as needed
-}
-
 //get all posts
 export const getAllPosts = async (req: Request, res: Response) => {
   try {
     const posts = await Post.find();
+    console.log(posts);
     res.status(200).json(posts);
   } catch (error: any) {
     res.status(404).json({ message: error.message });
@@ -40,7 +36,74 @@ export const getPostsByUserId = async (req: Request, res: Response) => {
   }
 };
 
+// get posts that are available
+export const getAvailablePosts = async (req: Request, res: Response) => {
+  try {
+    const posts = await Post.find({ status: "available" });
+    console.log(posts);
+    res.status(200).json(posts);
+  } catch (error: any) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+// get posts available by user id
+
+export const getAvailablePostsByUserId = async (
+  req: Request,
+  res: Response
+) => {
+  const id = req.params.id;
+  try {
+    const posts = await Post.find({ createdBy: id, status: "available" });
+    res.status(200).json(posts);
+  } catch (error: any) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+// get posts sold by user id
+
+export const getSoldPostsByUserId = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  try {
+    const posts = await Post.find({ createdBy: id, status: "sold" });
+    res.status(200).json(posts);
+  } catch (error: any) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+// get posts by genre
+
+export const getPostsByGenre = async (req: Request, res: Response) => {
+  // this might change to req.body
+  const id = req.params.id;
+  try {
+    const posts = await Post.find({ genres: id });
+    res.status(200).json(posts);
+  } catch (error: any) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+// get available posts where price is less than or equal to
+
+export const getPostsByPrice = async (req: Request, res: Response) => {
+  const price = req.params.price;
+  try {
+    const posts = await Post.find({
+      price: { $lte: price },
+      status: "available",
+    });
+    res.status(200).json(posts);
+  } catch (error: any) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
 //create post
+
 export const createPost = async (req: Request, res: Response) => {
   const { createdBy, image, author, title, genres, isbn, condition, price } =
     req.body;
@@ -81,6 +144,49 @@ export const createPost = async (req: Request, res: Response) => {
     res.status(201).json(newPost);
   } catch (error: any) {
     res.status(409).json({ message: error.message });
+  }
+};
+
+// update post general info
+
+export const updatePost = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const { image, author, title, genres, isbn, condition, price } = req.body;
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      {
+        image,
+        author,
+        title,
+        genres,
+        isbn,
+        condition,
+        price,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedPost);
+  } catch (error: any) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+// update post status (available or sold)
+export const updatePostStatus = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const { status } = req.body;
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      {
+        status,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedPost);
+  } catch (error: any) {
+    res.status(404).json({ message: error.message });
   }
 };
 
