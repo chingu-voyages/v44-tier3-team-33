495639043eb466479406b7586f6d6f2eebb8cc62
essss <sousououederni@gmail.com>
2023-05-18 13:10:58 +0100

    added cors

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import { LooseAuthProp } from "@clerk/clerk-sdk-node";
 import "dotenv/config";
 import { router } from "./routes/index.routes";
@@ -13,6 +14,7 @@ declare global {
 }
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 app.get("/", (req, res) => {
