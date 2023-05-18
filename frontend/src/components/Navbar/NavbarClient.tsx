132d2f9d1e6c1b72d299b924a521c9f9047dfeda
akshkin <107682289+akshkin@users.noreply.github.com>
2023-05-18 13:13:20 +0200

    Fix alignment issues when user is signed in
    On signing in there were some alignment issues which have now been fixed.

--- frontend/src/components/Navbar/NavbarClient.tsx
+++ frontend/src/components/Navbar/NavbarClient.tsx
@@ -16,17 +16,19 @@ function NavbarClient({ userId}: {userId: string | null}) {
   
   return (
     <>
-      <nav className={`flex  gap-4 ${isOpen ? "flex-col fixed left-0 top-[60px] xs:z-20 bg-white xs:shadow-lg p-4 w-full" : "hidden md:block md:flex-row md:w-auto "}`}>
-        <Link className="xs:ml-4 hover:opacity-60 focus:opacity:60  md:pr-4" href="">Discover</Link>
-        <Link className="xs:ml-4 hover:opacity-60 focus:opacity:60 md:pr-4" href="">Category</Link>
+      <nav className={`flex  gap-4 ${isOpen ? "flex-col fixed left-0 top-[60px] xs:z-20 bg-white xs:shadow-lg p-4 w-full" : "hidden md:inline-flex md:w-[65%] justify-end items-center"}`}>
+        {/* <div> */}
+          <Link className="xs:ml-4 hover:opacity-60 focus:opacity:60  md:pr-2" href="">Discover</Link>
+          <Link className="xs:ml-4 hover:opacity-60 focus:opacity:60 md:ml-0 md:pr-2" href="">Category</Link>
+        {/* </div> */}
         
         {userId ?
-          <>
-            <Link className="xs:ml-4 hover:opacity-60 focus:opacity:60" href="">Saved</Link>
-            <Link href=""><RxAvatar className="text-4xl" /></Link>
-            <Link href=""><BsCart3 className="text-3xl"/></Link>
-            <button className="xs:ml-4 hover:opacity-60 focus:opacity:60 text-left  md:pr-4"><UserButton /></button>
-          </> : 
+          <div className='flex flex-col md:flex-row gap-4 md:items-center'>
+            <Link className="xs:ml-4 md:ml-0 hover:opacity-60 focus:opacity:60" href="">Saved</Link>
+            <Link href=""><RxAvatar className="text-4xl xs:ml-4" /></Link>
+            <Link href=""><BsCart3 className="text-3xl xs:ml-4"/></Link>
+            <button className="xs:ml-4 hover:opacity-60 focus:opacity:60 text-left md:pr-4"><UserButton /></button>
+          </div> : 
             <button className="xs:ml-4 hover:opacity-60 focus:opacity:60 text-left  md:pr-4"><SignInButton  /></button>          
         }
       </nav> 
