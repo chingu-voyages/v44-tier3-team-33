f817949cef6b4ee7bd483ae2ba538e5510eaeaa1
essss <sousououederni@gmail.com>
2023-05-23 20:27:38 +0100

    added getCart

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -1,19 +1,35 @@
-import axios from "axios"
-import { PostType } from '@/types/post.types';
-import { UserType } from '@/types/user.types';
+import { PostType } from "@/types/post.types";
+import { UserType } from "@/types/user.types";
+import { currentUser } from "@clerk/nextjs";
+import axios from "axios";
 
-const url= "https://v44-tier3-team-33-u43g-heypbzh2c-eslemouederni.vercel.app/"
+const url =
+  "https://v44-tier3-team-33-u43g-heypbzh2c-eslemouederni.vercel.app/";
 
-export async function getPosts(){
+export async function getPosts() {
   try {
-    const response = await axios.get<{post:PostType, userInfo: UserType}[]>(`${url}/posts`)
-    if(response.status !== 200 , !response.data){
-        // This will activate the closest `error.js` Error Boundary
-        throw new Error('Failed to fetch data');
+    const response = await axios.get<{ post: PostType; userInfo: UserType }[]>(
+      `${url}/posts`
+    );
+    if ((response.status !== 200, !response.data)) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error("Failed to fetch data");
     }
-    return response.data
+    return response.data;
+  } catch (error: any) {
+    console.log(error);
+  }
+}
 
-  } catch(error: any){
-    console.log(error)
+export async function getCart() {
+  const user = await currentUser();
+  try {
+    const response = await axios.get(`${url}/carts/${user?.id}`);
+    if ((response.status !== 200, !response.data)) {
+      throw new Error("Failed to fetch data");
+    }
+    return response.data;
+  } catch (error: any) {
+    console.log(error);
   }
 }
