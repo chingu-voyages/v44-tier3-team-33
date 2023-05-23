6deba1dde23b30d5cd45f6eaf5abbda520e56852
essss <sousououederni@gmail.com>
2023-05-24 00:51:58 +0100

    added some details to cart page

--- frontend/src/app/cart/page.tsx
+++ frontend/src/app/cart/page.tsx
@@ -1,6 +1,5 @@
 import CartItem from "@/components/cart/CartItem";
 import { getCart } from "@/utils/utils";
-import { clerkClient } from "@clerk/nextjs";
 import React from "react";
 
 const page = async () => {
@@ -9,10 +8,22 @@ const page = async () => {
     return <div>The cart is currently empty </div>;
   }
   return (
-    <div className="flex w-full flex-col items-start justify-center gap-5 text-black">
-      {cart[0].posts.map((item: any) => (
-        <CartItem key={item._id} item={item} />
-      ))}
+    <div className="flex w-10/12 flex-col gap-2 text-black">
+      <div>
+        <h2 className="text-3xl">Your Cart</h2>
+        <hr className="w-full" />
+      </div>
+      <div className=" flex w-full flex-col items-center gap-1">
+        {cart[0].posts.map((item: any) => (
+          <CartItem key={item._id} item={item} />
+        ))}
+      </div>
+      <div className="w-full p-4">
+        <h6 className="text-right">
+          Total ({cart[0].posts.length} items) :{" "}
+          <span className="font-bold">{cart[0].totalPrice}.00$</span>
+        </h6>
+      </div>
     </div>
   );
 };
