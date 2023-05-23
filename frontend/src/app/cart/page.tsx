e37680085a7df5230846df203d71d5beae053c24
essss <sousououederni@gmail.com>
2023-05-24 00:22:18 +0100

    getting data

--- frontend/src/app/cart/page.tsx
+++ frontend/src/app/cart/page.tsx
@@ -1,10 +1,18 @@
 import CartItem from "@/components/cart/CartItem";
+import { getCart } from "@/utils/utils";
+import { clerkClient } from "@clerk/nextjs";
 import React from "react";
 
-const page = () => {
+const page = async () => {
+  const cart = await getCart();
+  if (!cart) {
+    return <div>The cart is currently empty </div>;
+  }
   return (
     <div className="flex w-full flex-col items-start justify-center gap-5 text-black">
-      <CartItem />
+      {cart[0].posts.map((item: any) => (
+        <CartItem key={item._id} item={item} />
+      ))}
     </div>
   );
 };
