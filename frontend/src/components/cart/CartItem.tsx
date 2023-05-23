fde7162179f58119e4a00c13a9ee0d8ec1ec1bcc
essss <sousououederni@gmail.com>
2023-05-24 00:52:08 +0100

    cart item has different info

--- frontend/src/components/cart/CartItem.tsx
+++ frontend/src/components/cart/CartItem.tsx
@@ -1,26 +1,41 @@
-import Image from "next/image";
-import React from "react";
+"use client";
 
-const CartItem = () => {
+import { CartType } from "@/types/cart.types";
+import { PostType } from "@/types/post.types";
+import { UserType } from "@/types/user.types";
+import { clerkClient } from "@clerk/nextjs";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { RiDeleteBinLine } from "react-icons/ri";
+
+const CartItem = ({ item }: { item: PostType }) => {
+  const { title, imgs, price, condition, description, isbn, createdBy } = item;
+
   return (
-    <div className="flex w-1/2 items-center justify-between border p-4 text-black">
+    <div className="flex w-full items-center justify-between border-b p-4 text-black">
       <div className=" rounded-md border border-t-fuchsia-950">
-        <Image src="" width={100} height={100} alt="product" />
+        <Image src={imgs?.[0]} width={150} height={150} alt="product" />
       </div>
       <div className="flex flex-col">
         <div>
-          <h2>Name of the book</h2>
+          <h2 className=" text-xl">{title}</h2>
         </div>
         <div>
-          <h3> Name of the seller</h3>
+          <h4>{description}</h4>
         </div>
         <div>
-          <h4>10.00$</h4>
+          <h4>Condition : {condition}</h4>
+        </div>
+        <div>
+          <h4>ISBN : {isbn}</h4>
+        </div>
+        <div>
+          <h4 className=" font-bold">{price}.00$</h4>
         </div>
       </div>
-      <div>
-        <button>Remove</button>
-      </div>
+      <button className=" flex items-center gap-1 rounded-md border px-4 py-2 text-black hover:bg-[#f5dcd7]">
+        <RiDeleteBinLine color="#EE4B2B" size={16} /> Remove
+      </button>
     </div>
   );
 };
