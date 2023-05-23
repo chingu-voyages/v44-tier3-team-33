0c80e1c069781d14b1facb56faa2b60b889aa0ec
essss <sousououederni@gmail.com>
2023-05-23 13:31:37 +0100

    get cart, totalPrice, add post, remove post

--- backend/routes/carts.routes.ts
+++ backend/routes/carts.routes.ts
@@ -2,4 +2,18 @@ import express from "express";
 
 const cartRouter = express.Router();
 
+import {
+  getCart,
+  addPostToCart,
+  removePostFromCart,
+  clearCart,
+  getCartTotalPrice,
+} from "../controllers/Cart.controller";
+
+cartRouter.get("/", getCart);
+cartRouter.post("/:id", addPostToCart);
+cartRouter.delete("/:id", removePostFromCart);
+cartRouter.delete("/", clearCart);
+cartRouter.get("/total-price", getCartTotalPrice);
+
 export { cartRouter };
