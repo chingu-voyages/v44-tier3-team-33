46b5dd9779e29847cc9b92f116d86ed5674f4493
Khaledtf Labeb <khaledtf19@gmail.com>
2023-05-27 21:39:52 +0300

    run build

--- backend/dist/utils/utils.js
+++ backend/dist/utils/utils.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getPostWithUser = exports.getPostsWithUser = void 0;
 const clerk_sdk_node_1 = require("@clerk/clerk-sdk-node");
-const getPostsWithUser = async ({ posts, }) => {
+const getPostsWithUser = async ({ posts }) => {
     return Promise.all(posts.map(async (post) => {
         const user = await clerk_sdk_node_1.users.getUser(post.createdBy);
         return {
@@ -19,21 +19,19 @@ const getPostsWithUser = async ({ posts, }) => {
     }));
 };
 exports.getPostsWithUser = getPostsWithUser;
-const getPostWithUser = async ({ post, }) => {
+const getPostWithUser = async ({ post }) => {
     const user = await clerk_sdk_node_1.users.getUser(post.createdBy);
-    return Promise.all([
-        {
-            post: post,
-            userInfo: {
-                id: user.id,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                email: user.emailAddresses[0].emailAddress,
-                profileImageUrl: user.profileImageUrl,
-                publicMetadata: user.publicMetadata,
-            },
+    return {
+        post: post,
+        userInfo: {
+            id: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.emailAddresses[0].emailAddress,
+            profileImageUrl: user.profileImageUrl,
+            publicMetadata: user.publicMetadata,
         },
-    ]);
+    };
 };
 exports.getPostWithUser = getPostWithUser;
 //# sourceMappingURL=utils.js.map
