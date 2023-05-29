6efefbec81471f54e07df3833f349f18bbf71bdd
essss <sousououederni@gmail.com>
2023-05-29 15:47:04 +0100

    added flex to discover page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,11 +9,13 @@ export default async function Home() {
   }
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-start p-4 md:p-8 ">
-      <h1 className="mb-8 mt-0 text-3xl font-bold">
-        Checkout books posted by sellers
-      </h1>
-      <PostGrid posts={posts} />
-    </div>
+    <main className="mt-[70px] flex min-h-screen flex-col items-center justify-start  gap-5 px-2 py-3 text-black md:mt-0">
+      <div className="flex min-h-screen flex-col items-center justify-start p-4 md:p-8 ">
+        <h1 className="mb-8 mt-0 text-3xl font-bold">
+          Checkout books posted by sellers
+        </h1>
+        <PostGrid posts={posts} />
+      </div>
+    </main>
   );
 }
