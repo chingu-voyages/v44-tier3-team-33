44f470945b4a03e1a7f5bc042564e147f6377ccf
akshkin <107682289+akshkin@users.noreply.github.com>
2023-06-07 22:35:04 +0200

    Add saving post feature
    Modify backend code

--- frontend/src/components/post/Post.tsx
+++ frontend/src/components/post/Post.tsx
@@ -1,11 +1,16 @@
+"use client";
+
 import { PostType } from "@/types/post.types";
 import { UserType } from "@/types/user.types";
+import { addToFavorites } from "@/utils/utils";
+import { useUser } from "@clerk/clerk-react";
+import { useAuth } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { AiOutlineHeart } from "react-icons/ai";
 
 import CartButton from "./CartButton";
-import Link from "next/link";
 
 function Post({
   postItem,
@@ -15,6 +20,16 @@ function Post({
 }) {
   const { userInfo, post } = postItem;
   const { title, imgs, price } = post;
+  const { userId, getToken } = useAuth();
+
+  const { user } = useUser();
+
+  async function addFavorites() {
+    const token = await getToken();
+    if (userId && token) {
+      await addToFavorites(post._id, userId, token);
+    }
+  }
 
   return (
     <div className="flex w-[250px] cursor-pointer flex-col items-center  justify-between rounded-xl border-2 border-gray-200 text-black md:w-[300px]">
@@ -40,9 +55,12 @@ function Post({
           {userInfo.firstName} {userInfo.lastName}
         </p>
         <div className="mt-2 flex flex-shrink justify-center text-black">
-          <div className="flex w-full items-center justify-center border-2  border-r-0  border-gray-300 px-8 py-2 text-2xl">
+          <button
+            onClick={addFavorites}
+            className="flex w-full items-center justify-center border-2  border-r-0  border-gray-300 px-8 py-2 text-2xl"
+          >
             <AiOutlineHeart />
-          </div>
+          </button>
           <div className="flex w-full items-center justify-center border-2 border-gray-300  text-2xl ">
             <CartButton postId={post._id} />
           </div>
