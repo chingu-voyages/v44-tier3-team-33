7d0b6399d3b88a9245f1fd458932afdc6975e858
Khaledtf Labeb <khaledtf19@gmail.com>
2023-06-07 22:02:16 +0300

    update post style, update loading, layout

--- frontend/src/app/loading.tsx
+++ frontend/src/app/loading.tsx
@@ -2,7 +2,7 @@ import { LoadingPostGrid } from "@/components/post/LoadingPost";
 
 const Loading = () => {
   return (
-     <div className="mt-[70px] flex min-h-screen flex-col items-center justify-start  gap-5 px-2 py-3 text-black md:mt-0">
+     <div className="flex min-h-screen flex-col items-center justify-start  gap-5 px-2 py-3 text-black ">
       <div className="flex min-h-screen flex-col items-center justify-start p-4 md:p-8 ">
         <h1 className="mb-8 mt-0 text-3xl font-bold">
           Checkout books posted by sellers
